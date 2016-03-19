60aa941f0eb2af26b1ef13a7b51535a9f35e4abd
Mielle Malcolm <malcolm.mielle@protonmail.com>
2016-03-19 22:08:02 +0100

    corrected Cmake installation
    Signed-off-by: Mielle Malcolm <malcolm.mielle@protonmail.com>

--- vodigrex-config.cmake
+++ vodigrex-config.cmake
@@ -1,3 +1,5 @@
+
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 # include(${SELF_DIR}/editdistance-targets.cmake)
 get_filename_component(vodigrex_INCLUDE_DIRS "${SELF_DIR}/../../include/vodigrex" ABSOLUTE)
+set(vodigrex_LIBRARIES "${SELF_DIR}/libVoronoi.a;${SELF_DIR}/libLineFollower.a")
